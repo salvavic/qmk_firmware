cfadf8160d8154abb25a7894591a5929551c5d13
Salvador Vicente de Valenzuela <salvadorvicente@gmail.com>
2021-04-14 22:17:12 +0200

    Inicio del tapdance

--- keyboards/ergodox_infinity/keymaps/salvavic/config.h
+++ keyboards/ergodox_infinity/keymaps/salvavic/config.h
@@ -4,8 +4,8 @@
 // #define UNICODE_SELECTED_MODES UC_LNX, UC_OSX, UC_WIN, UC_WINC
 // #define UNICODE_SELECTED_MODES UC_LNX
 // #define UNICODE_KEY_LNX LCTL(LSFT(KC_U))
-#undef tapping_term
-#define tapping_term 50
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
 
 
 #undef TAPPING_TOGGLE
--- keyboards/ergodox_infinity/keymaps/salvavic/rules.mk
+++ keyboards/ergodox_infinity/keymaps/salvavic/rules.mk
@@ -1,3 +1,4 @@
 # Switch to unicode map to allow getting the full unicode character space
 UNICODE_ENABLE = no
 UNICODEMAP_ENABLE = yes
+TAP_DANCE_ENABLE = yes
