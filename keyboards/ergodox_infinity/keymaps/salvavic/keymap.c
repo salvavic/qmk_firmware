ca480448a4ceca882e4cafba626129beb91f3f50
Salvador Vicente <salvadorvicente@gmail.com>
2021-04-14 15:05:12 +0200

    Pruebas con el block may

--- keyboards/ergodox_infinity/keymaps/salvavic/keymap.c
+++ keyboards/ergodox_infinity/keymaps/salvavic/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* BASE (QWERTY)
      *
      * ┌──────┬────┬────┬────┬────┬────┬────┐              ┌────┬────┬────┬────┬────┬────┬──────┐
-     * │ BACK │  1 │  2 │  3 │  4 │  5 │ ESC│              │ESC │  6 │  7 │  8 │  9 │  0 │  '   │
+     * │ ESC  │  1 │  2 │  3 │  4 │  5 │ ESC│              │ESC │  6 │  7 │  8 │  9 │  0 │  '   │
      * ├──────┼────┼────┼────┼────┼────┼────┤              ├────┼────┼────┼────┼────┼────┼──────┤
      * │  ESC │  Q │  W │  E │  R │  T │MOUS│              │MOUS│  Y │  U │  I │  O │  P │  `   │
      * ├──────┼────┼────┼────┼────┼────┤    │              │    ├────┼────┼────┼────┼────┼──────┤
@@ -121,20 +121,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                 │BACK│ INS│    │RAlt│RCtl│
      *                            ┌────┼────┼────┤    ├────┼────┼────┐
      *                            │    │    │Home│    │    │    │    │
-     *                            │ [] │ Del├────┤    ├────┤Entr│ [] │
+     *                            │BACK│ Del├────┤    ├────┤Entr│ [] │
      *                            │    │    │ End│    │MENU│    │    │
      *                            └────┴────┴────┘    └────┴────┴────┘
      */
     [_QWERTY] = LAYOUT_ergodox(  // QWERTY persistent layer
         // left hand
-        KC_BSPC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,    KC_ESC,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,    KC_ESC,
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,    TT(_MOUSE),
         KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,      KC_G,
         LSFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,    ES_MORD,
         KC_LCTL, KC_LGUI, KC_LALT, TT(_FN),  TT(_SIMBOLOS),
                                             KC_BSPC, KC_INS,
                                                        KC_HOME,
-                                   KC_SPC, KC_DEL,    KC_END,
+                                   KC_BSPC, KC_DEL,    KC_END,
         // right hand
         KC_ESC, KC_6,     KC_7,    KC_8,     KC_9,    KC_0,      ES_QUOT,
         TT(_MOUSE), KC_Y,     KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,
