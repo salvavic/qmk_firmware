5eac0189f82cd11b87c6f36aa4331e3b5dd9b268
Salvador Vicente de Valenzuela <salvadorvicente@gmail.com>
2021-05-05 18:53:49 +0200

    Cambios en config

--- keyboards/ergodox_infinity/keymaps/salvavic/config.h
+++ keyboards/ergodox_infinity/keymaps/salvavic/config.h
@@ -5,11 +5,20 @@
 // #define UNICODE_SELECTED_MODES UC_LNX
 // #define UNICODE_KEY_LNX LCTL(LSFT(KC_U))
 #undef TAPPING_TERM
-#define TAPPING_TERM 175
+// Configure the global tapping term (default: 200ms)
+
+#define TAPPING_TERM 200
+// Prevent normal rollover on alphas from accidentally triggering mods.
 
 #define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
 
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 1
 
 #define EE_HANDS
+
